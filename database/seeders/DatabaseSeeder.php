e1a23b1ef008929cc88007b9189e808ed8bbc537
asmi046 <asmi046@gmail.com>
2024-05-30 23:02:32 +0300

    выкладка на сайт

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Report::factory(40)->create();
+        // \App\Models\Report::factory(40)->create();
 
         // \App\Models\User::factory(10)->create();
 
